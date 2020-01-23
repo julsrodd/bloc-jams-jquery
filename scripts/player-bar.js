1875c86a7ce5d41b3384416744179ac825777201
Julianna <julianna.rodriguezd@gmail.com>
2020-01-22 22:27:32 -0500

    interval

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -26,6 +26,18 @@
      helper.playPauseAndUpdate(nextSong);
    });
 
+   $('#time-control input').on('input', function (event) {
+     player.skipTo(event.target.value);
+   });
+
+   setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
+     const duration = player.getDuration();
+     const percent = (currentTime / duration) * 100;
+     $('#time-control input').val(percent);
+   }, 1000);
+
   $('#volume-control input').on('input', function (event) {
     player.setVolume(event.target.value);
   });
